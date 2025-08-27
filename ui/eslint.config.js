d81a4b9e23befa3e3cf802d9cfb6f3b57eee936d
Matthew Meredith <matthewmeredith775@gmail.com>
2025-08-27 19:19:49 -0400

    reconfigure eslint

--- ui/eslint.config.js
+++ ui/eslint.config.js
@@ -1,27 +1,27 @@
-import pluginJs from "@eslint/js"
-import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
-import react from "eslint-plugin-react"
-import reactHooks from "eslint-plugin-react-hooks"
-import unusedImports from "eslint-plugin-unused-imports"
-import globals from "globals"
-import tseslint from "typescript-eslint"
+import pluginJs from "@eslint/js";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import react from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import unusedImports from "eslint-plugin-unused-imports";
+import globals from "globals";
+import tseslint from "typescript-eslint";
 
 export default [
   {
-    files: ["src/**/*.{js,mjs,cjs,ts,tsx}"]
+    files: ["src/**/*.{js,mjs,cjs,ts,tsx}"],
   },
   {
     languageOptions: {
       globals: {
-        ...globals.browser
+        ...globals.browser,
       },
       parser: tseslint.parser,
       parserOptions: {
         project: "./tsconfig.json",
         ecmaVersion: 2020,
-        sourceType: "module"
-      }
-    }
+        sourceType: "module",
+      },
+    },
   },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
@@ -30,15 +30,12 @@ export default [
   {
     plugins: {
       "unused-imports": unusedImports,
-      react: react
+      react: react,
     },
     rules: {
-      "@typescript-eslint/no-floating-promises": "error",
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-unused-vars": "off",
-      "@typescript-eslint/prefer-as-const": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
       "unused-imports/no-unused-imports": "warn",
-      "prettier/prettier": "warn"
-    }
-  }
-]
+      "prettier/prettier": "warn",
+    },
+  },
+];
